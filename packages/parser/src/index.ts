c5702a3dcc121f009b2b0b3622766a6a5684704f
Zachary Jones <zachary@jones.cx>
2020-04-16 11:26:16 -0400

    [parser] ♻️ Make default export a function
    This allows consumers to make an arbitrary amount of parser instances for their purposes without worrying about shared state issues.

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -2,6 +2,6 @@ import nearley from 'nearley';
 
 import grammar from './parser';
 
-const BeancountParser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+const BeancountParser = () => new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
 export default BeancountParser;
